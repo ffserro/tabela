5e269f2a0fd65384363c85fd9d32a3f9c08698b7
ffserro <ffserro@hotmail.com>
2025-01-07 07:59:58 -0300

    .

--- main.py
+++ main.py
@@ -70,6 +70,7 @@ for d in esc_preta.DATA.iloc[1:]:
     st.write(esc[6])
     st.write(esc[esc.index(esc_preta.loc[esc_preta.DATA == (d-td(1)), 'NOME'][0]) + 1])
     st.write(d)
+    st.write(esc_preta.loc[esc_preta.DATA==d])
     esc_preta.loc[esc_preta.DATA==d, 'NOME'] = esc[esc.index(esc_preta.loc[esc_preta.DATA == (d-td(1)), 'NOME'][0]) + 1]
 
 # for d in esc_vermelha[1:]:
