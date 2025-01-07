e81a14a880ae6f872789b9cb8ef8e31f6d63d879
ffserro <ffserro@hotmail.com>
2025-01-07 14:32:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,9 @@ esc_vermelha.set_index('DATA', inplace=True)
 for d in esc_preta.index[1:]:
     esc = get_disponivel(d, efetivo, restrito)
     esc = esc + [esc[0]]
+    st.write(d)
     st.write(esc)
+    st.write(preta)
     st.write(esc[esc.index(esc_preta.loc[preta[preta.index(d) - 1], 'NOME']) + 1])
     esc_preta.loc[d, 'NOME'] = esc[esc.index(esc_preta.loc[preta[preta.index(d) - 1], 'NOME']) + 1]
 
