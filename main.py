d5b0d2ba1dd3845c457206b00a72a0b71f0014b9
ffserro <ffserro@hotmail.com>
2025-01-07 05:51:39 -0300

    .

--- main.py
+++ main.py
@@ -38,5 +38,8 @@ for d in vermelha:
 
 vermelha.sort()
 
-st.write(vermelha)
-st.write(preta)
+for m in range(1, 13):
+    df = pd.DataFrame({'DIA':[d for d in datas if d.month() == m],
+                        'TABELA':['V' if d in vermelha else 'P' for d in datas if d.month() == m],
+                        'NOME':['' for d in datas if d.month() == m]})
+    st.session_state[meses[i]] = st.session_state.conn.update(worksheet=meses[i], data=df)
