3796d7f5881c1b2bfb0d60096b8aa211f648ffda
ffserro <ffserro@hotmail.com>
2025-01-07 12:34:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if action == 'Troca de serviço':
 gera_mes = st.selectbox('Gerar tabela do mês:', meses)
 if st.button('Gerar!') and gera_mes != '-':
     df = pd.DataFrame({'DIA': [d for d in datas if d.month == gera_mes], 'TABELA':['V' if d in vermelha else 'P' for d in datas if d.month == gera_mes], 'NOME':[geral_corrida.loc[d] for d in datas if d.month == gera_mes]})
-
+    st.session_state.conn.update(worksheet=gera_mes, data=df)
 # for m in range(1, 13):
 #     df = pd.DataFrame({'DIA':[d for d in datas if d.month == m],
 #                         'TABELA':['V' if d in vermelha else 'P' for d in datas if d.month == m],
