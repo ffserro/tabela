bbc1cb5a649e006a26c7b051966039c1086e8442
ffserro <ffserro@hotmail.com>
2025-01-07 08:10:04 -0300

    .

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ for d in esc_preta.index[1:]:
     esc = get_disponivel(d, efetivo, restrito)
     esc = esc + [esc[0]]
     st.write(preta[preta.index(d) - 1])
-    st.write(esc.index(esc_preta.loc[preta[preta.index(d)-1], 'NOME'][0]))
+    st.write(esc.index(esc_preta.loc[preta[preta.index(d) - 1], 'NOME'][0]))
     st.write(esc[6])
     st.write(esc[esc.index(esc_preta.loc[(d-td(1)), 'NOME'][0]) + 1])
     st.write(d)
