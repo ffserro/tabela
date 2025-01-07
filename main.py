4e77c54d470b1851954ae9e51f00ac6a126e93ba
ffserro <ffserro@hotmail.com>
2025-01-07 08:09:00 -0300

    .

--- main.py
+++ main.py
@@ -65,11 +65,10 @@ esc_vermelha.loc[esc_vermelha.DATA == dt(2025, 1, 1), 'NOME'] = 'CT Felipe Gondi
 
 esc_preta.set_index('DATA', inplace=True)
 
-st.write(esc_preta)
-
 for d in esc_preta.index[1:]:
     esc = get_disponivel(d, efetivo, restrito)
     esc = esc + [esc[0]]
+    st.write(preta.index(d))
     st.write(esc.index(esc_preta.loc[preta[preta.index(d)-1], 'NOME'][0]))
     st.write(esc[6])
     st.write(esc[esc.index(esc_preta.loc[(d-td(1)), 'NOME'][0]) + 1])
