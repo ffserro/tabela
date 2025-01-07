27f9cb4501e859fa443d7739f6245d0c1ea09320
ffserro <ffserro@hotmail.com>
2025-01-07 05:15:15 -0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+from datetime import datetime as dt
+from dateutil import tz
+
+tzinfo = tz.gettz('America/Sao_Paulo')
 
 st.title('TABELONA')
+
+st.session_state.conn = st.connection('gsheets', type=GSheetsConnection)
+
+st.session_state.efetivo = st.session_state.conn.read(worksheet='EMB')
+
+st.write(st.session_state.efetivo)
