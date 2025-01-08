0a78c0fd16dcb11df1715d9d5d287b2bc3de3065
ffserro <ffserro@hotmail.com>
2025-01-07 23:56:21 -0300

    .

--- main.py
+++ main.py
@@ -153,7 +153,8 @@ if action == 'Adicionar indisponibilidades':
         send_ind = st.form_submit_button('Enviar')
         if send_ind and mil_ind != '-':
             restrito = restrito_update()
-            restrito = pd.concat([pd.DataFrame({'NOME':[mil_ind], 'INICIAL':[per_ind[0]], 'FINAL':[per_ind[1]], 'MOTIVO':[mot_ind]}), restrito])
+            # restrito = pd.concat([pd.DataFrame({'NOME':[mil_ind], 'INICIAL':[per_ind[0]], 'FINAL':[per_ind[1]], 'MOTIVO':[mot_ind]}), restrito])
+            restrito.loc[len(restrito)] = [mil_ind, per_ind[0], per_ind[1], mot_ind]
             restrito = restrito.sort_values(by='INICIAL')
             st.session_state.conn.update(worksheet='REST', data=restrito)
 
