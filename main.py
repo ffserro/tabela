56cca338d3da54e561630b8358e3e740808543aa
ffserro <ffserro@hotmail.com>
2025-01-17 23:28:16 -0300

    .

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ troca['DE'] = pd.to_datetime(troca.DE, dayfirst=True)
 trocas_no_mes = troca[(troca.MOTIVO!='AUTOMÁTICA') & (troca.DE.dt.month ==gera_mes)]
 if gera_mes != 0 and len(troca[(troca.MOTIVO!='AUTOMÁTICA') & (troca.DE.dt.month == gera_mes)]) > 0:
     st.write(f'Trocas de {["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"][gera_mes]}:')
-    st.write(troca[(troca.MOTIVO!='AUTOMÁTICA') & (troca.DE.dt.month == gera_mes)])
+    st.dataframe(troca[(troca.MOTIVO!='AUTOMÁTICA') & (troca.DE.dt.month == gera_mes)], hide_index=True)
 if st.button('Gerar!') and gera_mes != 0:
     df = pd.DataFrame({'DIA': [d for d in datas if d.month == gera_mes], 'TABELA':['V' if d in vermelha else 'P' for d in datas if d.month == gera_mes], 'NOME':[geral_corrida.loc[pd.to_datetime(d)][0] for d in datas if d.month == gera_mes]})
     st.write(df)
