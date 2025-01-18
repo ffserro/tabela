e735eef02a33a0ba58423eda35153d325389c2e0
ffserro <ffserro@hotmail.com>
2025-01-17 22:42:39 -0300

    .

--- main.py
+++ main.py
@@ -169,10 +169,9 @@ with st.form('Trocas', clear_on_submit=True):
     st.write('Realizar trocas de serviço:')
     de = st.date_input('De:', dt.today())
     para = st.date_input('Para:', dt.today())
-    if de != para:
-        motivo_troca = st.text_input('Motivo da troca:')
-        geral_corrida.loc[de], geral_corrida.loc[para] = geral_corrida.loc[para], geral_corrida.loc[de]
-    if st.form_submit_button('Enviar'):
+    motivo_troca = st.text_input('Motivo da troca:')
+    geral_corrida.loc[de], geral_corrida.loc[para] = geral_corrida.loc[para], geral_corrida.loc[de]
+if st.form_submit_button('Enviar'):
         troca = pd.concat([troca, pd.DataFrame({'DE':[de], 'PARA':[para], 'MOTIVO':[motivo_troca]})])
         st.session_state.conn.update(worksheet='TROCA', data=troca)
 
