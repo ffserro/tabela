4c222557c5ea4c9bb6812ec8b965e6a0ae9a3bfd
ffserro <ffserro@hotmail.com>
2025-01-17 22:40:25 -0300

    .

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ with st.form('Trocas', clear_on_submit=True):
 st.divider()
 
 gera_mes = meses.index(st.selectbox('Gerar tabela do mês:', meses))
-st.write(troca)
+st.write(troca.DE.str.split('-'))
 trocas_no_mes = troca[(troca.MOTIVO!='AUTOMÁTICA') & (troca.DE.str.split('-')[1] == str(gera_mes))]
 if gera_mes != 0 and len(troca[(troca.MOTIVO!='AUTOMÁTICA') & (troca.DE.dt.month == gera_mes)]) > 0:
     st.write(f'Trocas de {["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"][gera_mes]}:')
