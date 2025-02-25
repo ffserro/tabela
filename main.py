e170cadf93ccde94c4b17e8f18370fd0bee3df55
ffserro <ffserro@hotmail.com>
2025-02-25 16:07:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -198,16 +198,13 @@ gera_mes = dt.today().month # meses.index(st.selectbox('Gerar tabela do mês:', 
 ### POROROCA
 carnaval = ['CT Tarle', '2T(IM) Soares Costa', 'CT Felipe Gondim', '1T Brenno Carvalho', 'SO-MO Alvarez', 'CT Damasceno', '1T Brenno Carvalho', 'CT(IM) Sêrro', 'CT Belmonte', '2T(IM) Soares Costa']
 for i in range(10):
-    st.write(geral_corrida.loc[pd.to_datetime(dt(2025,2,28) + td(days=i))])
     geral_corrida.loc[pd.to_datetime(dt(2025,2,28) + td(days=i))] = carnaval[i]
-
-st.write(geral_corrida.head())
 
 df1 = pd.DataFrame({'DIA': [d for d in datas if d.month == gera_mes], 'TABELA':['V' if d in vermelha else 'P' for d in datas if d.month == gera_mes], 'NOME':[geral_corrida.loc[pd.to_datetime(d)][0] for d in datas if d.month == gera_mes]})
 df2 = pd.DataFrame({'DIA': [d for d in datas if d.month == (gera_mes+1)%12], 'TABELA':['V' if d in vermelha else 'P' for d in datas if d.month == (gera_mes+1)%12], 'NOME':[geral_corrida.loc[pd.to_datetime(d)][0] for d in datas if d.month == (gera_mes+1)%12]})
 
-# df1.loc[dt(2025,2,28), 'TABELA'] = 'R'
-# df2.loc[(df2.DIA >= dt(2025,3,1)) & (df2.DIA <= dt(2025,3,9)), 'TABELA'] = 'R'
+df1.loc[dt(2025,2,28), 'TABELA'] = 'R'
+df2.loc[(df2.DIA >= dt(2025,3,1)) & (df2.DIA <= dt(2025,3,9)), 'TABELA'] = 'R'
 
 col1, col2 = st.columns(2)
 
