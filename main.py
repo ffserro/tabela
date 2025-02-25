5b2d6119d3a8d99be1204d311c1c9705a5f9513f
ffserro <ffserro@hotmail.com>
2025-02-25 20:42:51 -0300

    .

--- main.py
+++ main.py
@@ -206,6 +206,9 @@ df2 = pd.DataFrame({'DIA': [d for d in datas if d.month == (gera_mes+1)%12], 'TA
 df1.loc[df1.DIA==dt(2025,2,28), 'TABELA'] = 'R'
 df2.loc[(df2.DIA >= dt(2025,3,1)) & (df2.DIA <= dt(2025,3,9)), 'TABELA'] = 'R'
 
+df1['DIA'] = df1.DIA.dt.strftime('%dd/%mm/%YY')
+df2['DIA'] = df2.DIA.dt.strftime('%dd/%mm/%YY')
+
 col1, col2 = st.columns(2)
 
 with col1:
