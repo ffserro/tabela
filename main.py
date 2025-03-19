7d21ac8d5bffe797d1055e343025cb14d8ef5a40
ffserro <ffserro@hotmail.com>
2025-03-19 10:27:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ for d in esc_vermelha.index[1:]:
         esc_vermelha.loc[d, 'NOME'] = hoje[hoje.index(passa) - 1]
     else:
         try:
-            st.write(passa)
+            st.write(d, passa)
             st.write(hoje)
             st.write(ontem)
             esc_vermelha.loc[d, 'NOME'] = hoje[ontem.index(passa) - 1]
