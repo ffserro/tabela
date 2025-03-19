0284f6c728931583cfd33ef331099b1af61edd88
ffserro <ffserro@hotmail.com>
2025-03-19 13:43:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ meses = ['-', 'JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OU
 datas = [dt(ano, 1, 1) + td(i) for i in range(365)]
 
 #######
-datas = [i for i in datas if i.month in (dt.today().month(), (dt.today().month()-1)%12+1)]
+datas = [i for i in datas if i.month in (dt.today().month, (dt.today().month-1)%12+1)]
 
 feriados = holidays.Brazil()['{}-01-01'.format(ano): '{}-12-31'.format(ano)] + [dt(ano, 6, 11), dt(ano, 12, 13)]
 
