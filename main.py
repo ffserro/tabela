ff4fbc560b903709faeb3fb33bfd9fe166168576
ffserro <ffserro@hotmail.com>
2025-04-10 10:45:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ def restrito_update():
     st.session_state.restrito['INICIAL'] = pd.to_datetime(st.session_state.restrito['INICIAL'], dayfirst=True).dt.date
     st.session_state.restrito['FINAL'] = pd.to_datetime(st.session_state.restrito['FINAL'], dayfirst=True).dt.date
     st.write(st.session_state.restrito[st.session_state.restrito['MOTIVO']=='Férias'])
+    st.write(st.session_state.restrito.INICIAL)
+    st.write(st.session_state.restrito.INICIAL.apply(lambda x: x - td(days=1)))
     return st.session_state.restrito
 
 ano = 2025
